60a1a02faeb9f1d7e29969868c867049d3db4a6a
iosdevrookie <dev@iosdevrokiesMBP.fios-router.home>
2019-08-21 05:17:02 -0400

    multi stage dockerfile for go added

---
+++ Dockerfile.go
@@ -0,0 +1,27 @@
+FROM golang:1.12.0-alpine as builder
+
+RUN mkdir /app && apk add --no-cache git
+
+# copy all source files to app folder
+ADD cmd/ /app
+
+# make app the current directory all 
+# subsequent commands will be execute here
+
+WORKDIR /app
+
+# create the binary executable 
+
+RUN go install && go build -o main  
+
+#create smaller finalized image with binary from lighter image
+
+FROM alpine:latest as production
+
+# copy the app directory to our final image
+
+COPY --from=builder /app . 
+
+# run the binary
+
+CMD ["./main"]
