87b972ac4df88b9dc9aaeb77615cc572a1ce2e63
iosdevtrainee <43885360+iosdevtrainee@users.noreply.github.com>
2019-08-21 05:46:27 -0400

    Update Dockerfile.go
    Clarify explanations of the docker commands

--- Dockerfile.go
+++ Dockerfile.go
@@ -6,7 +6,7 @@ RUN mkdir /app && apk add --no-cache git
 ADD cmd/ /app
 
 # make app the current directory all 
-# subsequent commands will be execute here
+# subsequent commands will be executed here
 
 WORKDIR /app
 
@@ -15,10 +15,11 @@ WORKDIR /app
 RUN go install && go build -o main  
 
 #create smaller finalized image with binary from lighter image
+# using another base image i.e. alpine:latest  
 
 FROM alpine:latest as production
 
-# copy the app directory to our final image
+# copy the app directory from our build image to our final image
 
 COPY --from=builder /app . 
 
